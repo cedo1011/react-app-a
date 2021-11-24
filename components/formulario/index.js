77b3e82f9923f1201c2d550a770f6258d46758e1
cedo1011 <carloseduardodeoliveira11@gmail.com>
2021-11-24 12:26:26 +0000

    Uso de Estado

---
+++ components/formulario/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Text, View, TextInput, Button } from 'react-native';
+
+export default function Formulario () {
+    const [cep, onChangeCep] = useState('')
+    const [cidade, onChangeCidade] = useState('')
+
+    const handleConsultar = () => {
+        onChangeCidade(cep)
+    }
+
+    return(
+        <View>
+            <Text>Digite um CEP (somente números):</Text>
+            <TextInput value={cep} onChangeText={onChangeCep} />
+            <Button title="Consultar" onPress={handleConsultar} />
+            
+            <text>{cidade}</text>   
+        </View>
+    )
+}
